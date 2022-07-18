099386ca8f6453ffc9b5e7792ae62a8abd14c331
Rahi999 <101566073+Rahi999@users.noreply.github.com>
2022-07-18 07:53:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Github } from "./Components/Github";
+import { Login } from "./Components/Login";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Github /> */}
+      <Login />
+    </div>
+  );
+}
